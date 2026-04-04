f90f187ba36a9f24efd0bd8696161f33a521c91d
allo-les-anges <gm.harchies@gmail.com>
2026-04-04 13:57:30 +0200

    Fix structure and imports

--- src/app/[locale]/(agency_site)/[slug]/page.tsx
+++ src/app/[locale]/(agency_site)/[slug]/page.tsx
@@ -1,7 +1,12 @@
 import AgencyPageClient from "@/app/AgencyPageClient";
-export default function DynamicAgencyPage({ params }: { params: { slug: string } }) {
-  // Next.js récupère automatiquement "amaru-homes" depuis l'URL
-  const slug = params.slug;
+
+export default async function DynamicAgencyPage({ 
+  params 
+}: { 
+  params: Promise<{ slug: string }> 
+}) {
+  // On attend la résolution des paramètres (Obligatoire sur Next.js 15+)
+  const { slug } = await params;
 
   return <AgencyPageClient slug={slug} />;
 }
